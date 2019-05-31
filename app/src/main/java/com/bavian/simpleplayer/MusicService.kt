6bf044a022f3a36931462823451e9cebdfd7e37e
Ivan Bayev <ivan.baev.14@gmail.com>
2019-05-31 11:31:34 +0300

    New music service as been added

---
+++ app/src/main/java/com/bavian/simpleplayer/MusicService.kt
@@ -0,0 +1,37 @@
+package com.bavian.simpleplayer
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import com.bavian.simpleplayer.player.Player
+import com.bavian.simpleplayer.player.compositions.LocalCompositionsList
+
+class MusicService: Service() {
+
+    companion object {
+        var player: Player? = null
+            private set
+
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        val result = super.onStartCommand(intent, flags, startId)
+
+        if (player == null) {
+            player = Player(LocalCompositionsList(intent!!.extras!!.getString("path")!!))
+        }
+
+        return result
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        player?.off()
+    }
+
+}

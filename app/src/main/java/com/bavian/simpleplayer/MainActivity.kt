3459ea60be43fd62cc96b05f65e12859db838e33
Ivan Bayev <ivan.baev.14@gmail.com>
2019-06-02 14:49:59 +0300

    Mini-player work has been setting

--- app/src/main/java/com/bavian/simpleplayer/MainActivity.kt
+++ app/src/main/java/com/bavian/simpleplayer/MainActivity.kt
@@ -3,20 +3,84 @@ package com.bavian.simpleplayer
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.GestureDetector
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var timerHandler: Handler? = null
+    private var timerUpdater: Runnable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         launchGestureListener()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val miniPlayer = findViewById<LinearLayout>(R.id.mini_player)
+
+        if (MusicService.player?.isPlaying() == true) {
+            miniPlayer.visibility = View.VISIBLE
+        } else {
+            miniPlayer.visibility = View.GONE
+        }
+
+        launchCompositionAnalyser()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        timerHandler?.removeCallbacks(timerUpdater)
     }
 
     fun chooseDirectory(view: View) {
         val intent = Intent(this, DirectoryChooser::class.java)
         startActivity(intent)
+    }
+
+    fun pause(view: View) {
+        MusicService.player?.pause()
+        findViewById<LinearLayout>(R.id.mini_player).visibility = View.GONE
+    }
+
+    fun next(view: View) {
+        MusicService.player?.next()
+    }
+
+    private fun launchCompositionAnalyser() {
+
+        val timer = findViewById<TextView>(R.id.rest_time)
+
+        val name = findViewById<TextView>(R.id.name)
+
+        val timerHandler = Handler()
+        val timerUpdater = object : Runnable {
+
+            override fun run() {
+                val progress = MusicService.player?.progress
+                val duration = MusicService.player?.duration
+
+                val restTime = "-${getTimer(duration?.minus(progress ?: 0))}"
+                timer.text = restTime
+
+                name.text = MusicService.player?.currentCompositionName ?: "Unknown"
+
+                timerHandler.postDelayed(this, 1000)
+            }
+
+        }
+
+        timerHandler.postDelayed(timerUpdater, 0)
+
+        this.timerHandler = timerHandler
+        this.timerUpdater = timerUpdater
     }
 
     private fun launchGestureListener() {
@@ -27,6 +91,20 @@ class MainActivity : AppCompatActivity() {
             gdt.onTouchEvent(event)
             true
         }
+    }
+
+    fun getTimer(time: Int?): String {
+
+        if (time == null) {
+            return "0:00"
+        }
+
+        val seconds = time / 1000 % 60
+        val minutes = time / 1000 / 60
+
+        val secondsString = if (seconds < 10) "0$seconds" else seconds.toString()
+
+        return "$minutes:$secondsString"
     }
 
 }

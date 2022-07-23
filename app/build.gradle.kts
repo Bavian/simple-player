9fb4e04a4223d1b1eeac5894a6685ed1da2912a5
Ivan Bayev <ivan.baev.14@gmail.com>
2022-07-23 23:24:22 +0300

    jetpack-compose: setup build files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,30 @@
 plugins {
     id("com.android.application")
     kotlin("android")
-    kotlin("android.extensions")
 }
 
 android {
-    compileSdkVersion(33)
+    compileSdk = 33
     defaultConfig {
         applicationId = "com.bavian.simpleplayer"
-        minSdkVersion(22)
-        targetSdkVersion(33)
+        minSdk = 21
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildFeatures {
+        compose = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.1"
     }
     buildTypes {
         getByName("release") {
@@ -24,11 +36,33 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}")
     implementation("androidx.appcompat:appcompat:${Versions.AndroidX.appCompat}")
     implementation("androidx.constraintlayout:constraintlayout:${Versions.AndroidX.constraintLayout}")
     implementation("androidx.legacy:legacy-support-v4:${Versions.AndroidX.legacySupportV4}")
     testImplementation("junit:junit:${Versions.junit}")
     androidTestImplementation("androidx.test.ext:junit:${Versions.AndroidX.junit}")
     androidTestImplementation("androidx.test.espresso:espresso-core:${Versions.AndroidX.espresso}")
+
+    // Jetpack Compose
+    // Integration with activities
+    implementation("androidx.activity:activity-compose:${Versions.JetpackCompose.activity}")
+    // Compose Material Design
+    implementation("androidx.compose.material:material:${Versions.JetpackCompose.extra}")
+    // Animations
+    implementation("androidx.compose.animation:animation:${Versions.JetpackCompose.extra}")
+    // Tooling support (Previews, etc.)
+    debugImplementation("androidx.compose.ui:ui-tooling:${Versions.JetpackCompose.extra}")
+    implementation("androidx.compose.ui:ui-tooling-preview:${Versions.JetpackCompose.extra}")
+    implementation("androidx.compose.ui:ui:${Versions.JetpackCompose.extra}")
+    // Tooling support (Previews, etc.)
+    implementation("androidx.compose.ui:ui-tooling:${Versions.JetpackCompose.extra}")
+    // Foundation (Border, Background, Box, Image, Scroll, shapes, animations, etc.)
+    implementation("androidx.compose.foundation:foundation:${Versions.JetpackCompose.extra}")
+    // Material Design
+    implementation("androidx.compose.material:material:${Versions.JetpackCompose.extra}")
+    // Material design icons
+    implementation("androidx.compose.material:material-icons-core:${Versions.JetpackCompose.extra}")
+    implementation("androidx.compose.material:material-icons-extended:${Versions.JetpackCompose.extra}")
+
 }
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val gradle = "7.2.1"
-    const val kotlin = "1.7.10"
+    const val kotlin = "1.6.10"
     const val junit = "4.12"
 
     object AndroidX {
@@ -9,5 +9,10 @@ object Versions {
         const val junit = "1.1.3"
         const val espresso = "3.4.0"
         const val legacySupportV4 = "1.0.0"
+    }
+
+    object JetpackCompose {
+        const val activity = "1.5.0"
+        const val extra = "1.1.1"
     }
 }

9a982b9c8266686b7abb1f748c8a0bff3e8cd4ff
Ivan Bayev <ivan.baev.14@gmail.com>
2022-07-23 14:46:25 +0300

    upgrade: migrate to AndroidX

--- app/src/main/java/com/bavian/simpleplayer/DirectoryChooser.kt
+++ app/src/main/java/com/bavian/simpleplayer/DirectoryChooser.kt
@@ -3,11 +3,11 @@ package com.bavian.simpleplayer
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter

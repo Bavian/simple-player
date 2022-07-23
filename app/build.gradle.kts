60865c3ebf2381a0f003b5c7798ea9d339ff1641
Ivan Bayev <ivan.baev.14@gmail.com>
2022-07-23 20:31:06 +0300

    kts-migration: declare versions in a separate file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,10 +25,10 @@ android {
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}")
-    implementation("androidx.appcompat:appcompat:1.4.2")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.12")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.appcompat:appcompat:${Versions.AndroidX.appCompat}")
+    implementation("androidx.constraintlayout:constraintlayout:${Versions.AndroidX.constraintLayout}")
+    implementation("androidx.legacy:legacy-support-v4:${Versions.AndroidX.legacySupportV4}")
+    testImplementation("junit:junit:${Versions.junit}")
+    androidTestImplementation("androidx.test.ext:junit:${Versions.AndroidX.junit}")
+    androidTestImplementation("androidx.test.espresso:espresso-core:${Versions.AndroidX.espresso}")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("com.android.tools.build:gradle:${Versions.gradle}")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
